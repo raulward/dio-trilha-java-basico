f62d7b67ceabfc39c7f2b922fc688c58d25fa9e8
raulward <raulwarddev@gmail.com>
2024-07-17 01:23:54 -0300

    :sparkles: feat: Adding ContaTerminal totally functional

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o número da agencia: ");
+        String agenceNumber = scanner.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Digite o saldo bancário: ");
+        float salary = scanner.nextFloat();
+
+        String message = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", name, agenceNumber, accountNumber, salary);
+
+        System.out.println(message);
+
+        
     }
 }
